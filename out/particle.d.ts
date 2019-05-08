2380521f527d613abac4366e787563eef0e7ded6
Karan Jit Singh <karsin@microsoft.com>
2019-05-08 15:33:48 +0530

    fix declarations

--- out/particle.d.ts
+++ out/particle.d.ts
@@ -16,6 +16,32 @@ declare class Atom implements DrawObject {
     constructor(id: number, speed?: Point, position?: Point, opacity?: number, options?: AtomDrawOptions);
     draw(context: ParticleJSContext): void;
 }
+interface AtomDrawOptions {
+    pop: boolean;
+    blur: boolean;
+    popRadius: number;
+    popProbability: number;
+    radius: number;
+    colorSet: Array<string>;
+}
+interface ParticleJSOptions {
+    drawCanvasBackground: boolean;
+    canvasBGColor: string;
+    animationInterval?: number;
+    beforeDraw?: (ctx: CanvasRenderingContext2D) => void;
+    afterDraw?: (ctx: CanvasRenderingContext2D) => void;
+}
+interface ParticleJSContext {
+    canvasContext: CanvasRenderingContext2D;
+    mousePosition: Point;
+    canvasHeight: number;
+    canvasWidth: number;
+    options: ParticleJSOptions;
+}
+interface DrawObject {
+    draw(context: ParticleJSContext): void;
+    dispose(): Array<Atom>;
+}
 declare class ParticleJS {
     static default: ParticleJSOptions;
     private ctx;
